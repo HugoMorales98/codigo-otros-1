170b23b433dc877344cf3af8f1dbffb5700bcd02
HugoMorales <hugo.156@hotmail.com>
2023-03-21 19:13:15 -0600

    Codigo app.js arreglado

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,19 +4,20 @@ const $n = document.querySelector('name');
 const $b = document.querySelector('#blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username) { // se debe agregar "async" antes del parámetro de la función
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  console.log(response); // se debe usar la variable "response" en lugar de "data"
+  const data = await response.json(); // se debe esperar la respuesta en formato JSON
+  $n.textContent = `${data.name}`; // se deben usar comillas invertidas en lugar de comillas simples
+  $b.textContent = `${data.blog}`; // se deben usar comillas invertidas en lugar de comillas simples
+  $l.textContent = `${data.location}`; // se deben usar comillas invertidas en lugar de comillas simples
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`; // se debe usar "$n" en lugar de "n"
 }
 
 displayUser('stolinski').catch(handleError);
